f2d06c34594a7284ed0f181320076202d78df54b
andrew-dev-p <popov.andrew.dev@gmail.com>
2025-02-21 17:15:00 +0200

    fix: Handle error state in GameGrid component and streamline loading skeleton rendering

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -13,31 +13,30 @@ interface GameGridProps {
 export const GameGrid: React.FC<GameGridProps> = ({ gameQuery }) => {
   const { data: games, error, isLoading } = useGames(gameQuery);
 
+  if (error) return <Text>{error}</Text>;
+
   return (
-    <>
-      {error && <Text>{error}</Text>}
-      <SimpleGrid
-        padding="10px"
-        columns={{
-          sm: 1,
-          md: 2,
-          lg: 3,
-          xl: 4,
-        }}
-        gap={5}
-      >
-        {games.map((game) => (
-          <GameCardContainer key={game.id}>
-            <GameCard game={game} />
+    <SimpleGrid
+      padding="10px"
+      columns={{
+        sm: 1,
+        md: 2,
+        lg: 3,
+        xl: 4,
+      }}
+      gap={5}
+    >
+      {games.map((game) => (
+        <GameCardContainer key={game.id}>
+          <GameCard game={game} />
+        </GameCardContainer>
+      ))}
+      {isLoading &&
+        Array.from({ length: 6 }).map((_, index) => (
+          <GameCardContainer key={index}>
+            <GameCardSkeleton />
           </GameCardContainer>
         ))}
-        {isLoading &&
-          Array.from({ length: 6 }).map((_, index) => (
-            <GameCardContainer key={index}>
-              <GameCardSkeleton />
-            </GameCardContainer>
-          ))}
-      </SimpleGrid>
-    </>
+    </SimpleGrid>
   );
 };
